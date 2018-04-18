cbf703027e2b0f3ae6d1bbfc3b42bf3c9076e0bd
sbmpost <s.post@thenewmotion.com>
2018-04-18 22:54:38 +0200

    Get rid of unnecessary files

--- go.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-verilator -CFLAGS -DVL_DEBUG=0 -cc LogisimToplevelShell.v --exe shell.cpp
-make -j -C obj_dir -f VLogisimToplevelShell.mk && obj_dir/VLogisimToplevelShell
--- go_top.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-verilator -CFLAGS -DVL_DEBUG=0 -cc top.v --exe top.cpp
-make -j -C obj_dir -f Vtop.mk && obj_dir/Vtop
